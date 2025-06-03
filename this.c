22364b9e156209f125cf15d94bd2c0e936880e73
Nikolas Neofytou <nikiforialicht@gmail.com>
2025-06-03 22:56:46 +0300

    testing the repo

---
+++ this.c
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+int main () {
+    cout << "what is the point of the lecture if it is not to entertain the people and the masses " << endl;
+
+
+
+
+
+    return 0;
+}
